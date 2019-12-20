0a66d1b0096438db04a2c0f09c3b0a158ce922a5
Kozlovtseva <alina.lm@mail.ru>
2019-12-20 09:35:40 +0300

    add Layout component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
-// import Layout from "./Layout";
+import Layout from "./Layout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Provider store={store}>{/* <Layout /> */}</Provider>
+        <Provider store={store}>
+          <Layout />
+        </Provider>
       </div>
     );
   }
---
+++ src/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Constructor from "./containers/Constructor";
+import Header from "./components/Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Constructor />
+    </div>
+  );
+};
+
+export default Layout;
