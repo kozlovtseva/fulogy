c2a29513f17ab4fbda1d56317b0b4d4af1fe3ae9
Kozlovtseva <alina.lm@mail.ru>
2019-12-20 15:15:36 +0300

    style Info component

--- src/components/constructor/Info.js
+++ src/components/constructor/Info.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-// import styles from "./Info.module.css";
+import styles from "./Info.module.css";
 
 const Info = props => {
-  return <div className="Info">{props.data.sum}</div>;
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Item}>
+        <p className={styles.Title}>Класс:</p>
+        <div className={styles.Class + " " + styles.Text}>
+          {props.data.class}
+        </div>
+      </div>
+      <div className={styles.Item}>
+        <p className={styles.Title}>Потребляемая мощность:</p>
+        <p className={styles.Text}>{props.data.capacity}</p>
+      </div>
+      <div className={styles.Item}>
+        <p className={styles.Title}>Сила света:</p>
+        <p className={styles.Text}>{props.data.intensity}</p>
+      </div>
+      <div className={styles.Item}>
+        <p className={styles.Title}>Гарантия:</p>
+        <p className={styles.Text}>{props.data.guaranty}</p>
+      </div>
+      <div className={styles.Item}>
+        <p className={styles.Title}>Монтаж:</p>
+        <p className={styles.Text}>{props.data.installation}</p>
+      </div>
+      <div className={styles.Item}>
+        <p className={styles.Title}>Итого сумма:</p>
+        <p className={styles.Text}>{props.data.sum}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
